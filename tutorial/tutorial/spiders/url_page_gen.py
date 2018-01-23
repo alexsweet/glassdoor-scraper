3f80423f56add9260606dcf17e19cd224f614729
swee0202 <swee0202@umn.edu>
2018-01-23 16:04:28 -0600

    Changed folder names

--- tutorial/tutorial/spiders/url_page_gen.py
+++
@@ -1,9 +0,0 @@
-def url_page_maker(urls,n):
-    url_list = []
-    for url in urls:
-        index = url.find(".htm")
-        for i in range(2,n+1):
-            url_nxt_page = url[0:index]+"_P"+str(i)+".htm"
-            url_list.append(url_nxt_page)
-    
-    return url_list
